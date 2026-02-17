7e761df5f02918c23d4a300a24d77024b2d8e9b4
ABHIJAL C A <abhijalchalliyil07@gmail.com>
2026-02-17 13:34:48 +0530

    Add files via upload

---
+++ ReverseNumbers.java
@@ -0,0 +1,9 @@
+// Prints numbers from 10 down to 1 using a loop.
+
+public class ReverseNumbers {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
